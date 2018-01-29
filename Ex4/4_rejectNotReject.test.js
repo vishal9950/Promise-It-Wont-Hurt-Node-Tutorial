60d9018884f845da241660ab8e4b578ab441af29
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-01-30 00:23:16 +0530

    Ex4: #TC2: rejects async func with error

--- Ex4/4_rejectNotReject.test.js
+++ Ex4/4_rejectNotReject.test.js
@@ -4,4 +4,8 @@ describe('Functionality of promise: ', () => {
   test('Should resolve the async func: ', () => {
     expect(exp.promise).resolves.toBe('Hello');
   });
+
+  test('Should reject the async func with error: ', () => {
+    expect(exp.promise).rejects.toBe('error');
+  });
 });
