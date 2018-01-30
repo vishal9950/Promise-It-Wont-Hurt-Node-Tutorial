61273aac5772f6e716c6bc5971284eb1ad57fd12
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-01-30 15:11:01 +0530

    Ex7: #TC2: second promise returns a promise

--- Ex7/7_chainPromise.test.js
+++ Ex7/7_chainPromise.test.js
@@ -1,5 +1,13 @@
 const exp = require('./7_chainPromise');
 
 describe('Test firstPromise: ', () => {
-  test('Should return a promise: ', () => expect(exp.firstPromise).toBeInstanceOf(Promise));
+  test('Should return a promise: ', () => {
+    expect(exp.firstPromise).toBeInstanceOf(Promise);
+  });
+});
+
+describe('Test secondPromise: ', () => {
+  test('Should return a promise: ', () => {
+    expect(exp.secondPromise).toBeInstanceOf(Promise);
+  });
 });
