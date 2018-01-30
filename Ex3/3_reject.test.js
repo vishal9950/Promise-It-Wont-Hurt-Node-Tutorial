c1c51daee0fbb856d15194c76423a646c881a027
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-01-30 09:16:37 +0530

    Ex3: Test cases refactored

--- Ex3/3_reject.test.js
+++ Ex3/3_reject.test.js
@@ -1,21 +1,11 @@
 const exp = require('./3_reject');
 
 describe('Functionality of promise: ', () => {
-  test('Should resolve the async func: ', () => {
-    expect(exp.promise).resolves.toBe('Hello');
-  });
-
-  test('Should reject the async func with error: ', () => {
-    expect(exp.promise).rejects.toBe('error');
-  });
+  test('Should resolve the async func: ', () => expect(exp.promise).rejects.toEqual(new Error('REJECTED!')));
 });
 
 describe('Functionality of onReject: ', () => {
   global.console.log = jest.fn();
-  test('Should be called 1 time(s): ', () => {
-    exp.onReject('Hello');
-    expect(console.log).toHaveBeenCalledTimes(1);
-  });
 
   test('Should be called with \'Hello\': ', () => {
     exp.onReject(new Error('Hello'));
