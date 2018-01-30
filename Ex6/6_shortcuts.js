c4faae3bd6e765db0b28b1d6c2b705396fc22e1a
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-01-30 12:40:01 +0530

    Ex6: #TC2: rejects with reject error msg

--- Ex6/6_shortcuts.js
+++ Ex6/6_shortcuts.js
@@ -1,5 +1,14 @@
 const promiseResolve = Promise.resolve('SOME VALUE!');
 
+const promiseReject = Promise.reject(new Error('Error msg'));
+
+promiseReject.catch((err) => {
+  console.error(err.message);
+});
+
 promiseResolve.then(console.log);
 
-module.exports = promiseResolve;
+module.exports = {
+  promiseResolve,
+  promiseReject,
+};
--- Ex6/6_shortcuts.test.js
+++ Ex6/6_shortcuts.test.js
@@ -1,5 +1,9 @@
-const promiseResolve = require('./6_shortcuts');
+const exp = require('./6_shortcuts');
 
 describe('Test resolve: ', () => {
-  test('Should resolve with resolve value: ', () => expect(promiseResolve).resolves.toBe('SOME VALUE!'));
+  test('Should resolve with resolve value: ', () => expect(exp.promiseResolve).resolves.toBe('SOME VALUE!'));
+});
+
+describe('Test reject: ', () => {
+  test('Should reject with error object as value: ', () => expect(exp.promiseReject).rejects.toEqual(new Error('Error msg')));
 });
